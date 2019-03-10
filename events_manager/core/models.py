9d94479deeb4fc0d184b666013db68c3beb96904
Juan David <juandavidblues@gmail.com>
2019-03-10 12:18:16 -0500

    Creacion de BaseModel y ajustes en el modelo de eventos segun lo propuesto en clase, falta migrar

--- events_manager/core/models.py
+++ events_manager/core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser,Permission
 from django.contrib.contenttypes.models import ContentType
+from django.utils.timezone import now
 
 class User(AbstractUser):
 
@@ -94,3 +95,35 @@ User._meta.get_field('first_name').verbose_name = 'Nombre'
 User._meta.get_field('last_name').verbose_name = 'Apellido'
 User._meta.get_field('is_active').verbose_name = 'Activo'
 User._meta.get_field('is_active').help_text = 'Desactiva el acceso al usuario a las caracteristicas del sistema'
+
+class BaseModel(models.Model):
+    detail_view_name = None
+    edit_view_name = None
+    delete_view_name = None
+
+
+    user_creator = models.ForeignKey(to=User,related_name='user_creator',on_delete=models.CASCADE,verbose_name='Usuario Creador')
+    is_active = models.BooleanField(default=True,verbose_name='Activo')
+    creation_date = models.DateTimeField(default=now,verbose_name='Fecha De Creación')
+
+    @property
+    def get_absolute_detail_url(self):
+        from django.urls import reverse_lazy
+        return reverse_lazy(self.detail_view_name, args=[str(self.id)])
+
+    @property
+    def get_absolute_edit_url(self):
+        from django.urls import reverse_lazy
+        return reverse_lazy(self.edit_view_name, args=[str(self.id)])
+
+    @property
+    def get_absolute_delete_url(self):
+        from django.urls import reverse_lazy
+        return reverse_lazy(self.delete_view_name, args=[str(self.id)])
+
+    def delete(self,*args, **kwargs):
+        self.is_active = False
+        self.save()
+
+    class Meta:
+        abstract = True
--- events_manager/event/models.py
+++ events_manager/event/models.py
@@ -1,56 +1,44 @@
 from django.db import models
 from django.utils.timezone import now
+from events_manager.core.models import BaseModel
+from django.core.exceptions import ValidationError
 
-# Create your models here.
-class Event(models.Model):
+def validator_greater_zero(value):
+    if value < 0:
+        raise ValidationError(
+            'El Valor Debe Ser Mayor A 0'
+        )
 
-    TYPES = (
-        ('Futbol','Futbol'),
-        ('Tenis ','Tenis '),
-        ('Béisbol','Béisbol')
-    )
 
-    DIVISIONS = (
-        ('Primera','Primera'),
-        ('Segunda ','Segunda '),
-        ('Tercera','Tercera')
-    )
+class TypeEvent(BaseModel):
+    name = models.CharField(max_length=250,blank=False,null=False,verbose_name='Nombre')
+    description = models.TextField(blank=True,null=True,verbose_name='Descripcion')
+
+    def __str__(self):
+        return self.name
+
+class Locality(BaseModel):
+    name = models.CharField(max_length=250,blank=False,null=False,verbose_name='Nombre')
+    description = models.TextField(blank=True,null=True,verbose_name='Descripcion')
+
+class Event(BaseModel):
+    self.detail_view_name = 'event:detail'
+    self.edit_view_name = 'event:update'
+    self.delete_view_name = 'event:delete'
 
     name = models.CharField(max_length=250,blank=False,null=False,verbose_name='Nombre')
     descripcion = models.TextField(blank=True,null=True,verbose_name='Descripcion')
-    event_type = models.CharField(max_length=250,blank=False,null=False,verbose_name='Tipo',choices=TYPES)
-    capacity = models.IntegerField(blank=False,null=False,default=0,verbose_name='Capacidad')
-    date = models.DateField(blank=False,null=False,default=now,verbose_name='Fecha')
-    cupos_restantes = models.IntegerField(blank=False,null=False,default=0,verbose_name='Cupos Restantes')
-    division = models.CharField(max_length=250,blank=False,null=True,verbose_name='Division',choices=DIVISIONS)
-    equipo_local = models.CharField(max_length=250,blank=False,null=True,verbose_name='Equipo Local')
-    equipo_visitante = models.CharField(max_length=250,blank=False,null=True,verbose_name='Equipo Visitante')
-    precio_ticket = models.IntegerField(blank=False,null=False,default=0,verbose_name='Precio Boleto')
-    is_active = models.BooleanField(default=True,verbose_name='Activo')
-
-    @property
-    def get_absolute_detail_url(self):
-        from django.urls import reverse_lazy
-        return reverse_lazy('event:detail', args=[str(self.id)])
-
-    @property
-    def get_absolute_edit_url(self):
-        from django.urls import reverse_lazy
-        return reverse_lazy('event:update', args=[str(self.id)])
-
-    @property
-    def get_absolute_delete_url(self):
-        from django.urls import reverse_lazy
-        return reverse_lazy('event:delete', args=[str(self.id)])
-
-    def save(self,*args, **kwargs):
-        if not self.pk:
-            self.cupos_restantes = self.capacity
-        super(Event, self).save(*args, **kwargs)
-
-    def delete(self,*args, **kwargs):
-        self.is_active = False
-        self.save()
+    event_type = models.ForeignKey(to=TypeEvent,on_delete=models.CASCADE,blank=False,null=True,verbose_name='Tipo De Evento',related_name='event_type')
+    date = models.DateField(blank=False,null=False,default=now,verbose_name='Fecha Evento')
 
     def __str__(self):
-        return self.name + ': ' + self.equipo_local + ' VS ' + self.equipo_visitante
+        return self.name
+
+class EventLocality(BaseModel):
+    event = models.ForeignKey(to=Event,on_delete=models.CASCADE,blank=False,null=True,verbose_name='Evento',related_name='event')
+    locality = models.ForeignKey(to=Event,on_delete=models.CASCADE,blank=False,null=True,verbose_name='Localidad',related_name='locality')
+    capacity = models.IntegerField(blank=False,null=False,default=0,verbose_name='Capacidad',validators=[validator_greater_zero])
+    price = models.FloatField(blank=False,null=False,default=0,verbose_name='Capacidad',validators=[validator_greater_zero])
+
+
+    
